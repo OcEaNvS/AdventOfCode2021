9a8528d7487f0bbe25c22cae7c1913e76bd11af4
Orhan Yigit Yazicilar <oyy1@st-andrews.ac.uk>
2021-12-10 15:13:09 +0000

    Made Day 10 Shorter

--- Day10/brackets.rs
+++ Day10/brackets.rs
@@ -1,102 +1,48 @@
 use std::fs;
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 fn main() {
     let input = fs::File::open("input.txt").expect("File not found");
-    let lines = io::BufReader::new(input).lines().map(|l| l.unwrap()).collect::<Vec<_>>();
-    let mut corrupted_line = vec![false; lines.len()];
+    let lines = io::BufReader::new(input).lines().map(|l| l.unwrap());
+    let part1_points: HashMap<char, usize> = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)].iter().cloned().collect();
+    let part2_points: HashMap<char, usize> = [('(', 1), ('[', 2), ('{', 3), ('<', 4)].iter().cloned().collect();
+    let matching_bracket: HashMap<char, char> = [(')', '('), (']', '['), ('}', '{'), ('>', '<')].iter().cloned().collect();
     let mut part1_total = 0;
     let mut part2_total_vec = Vec::new();
-    let mut open_bracket_vector = Vec::new();
 
-    for i in 0..lines.len() {
-        for c in lines[i].chars() {
+    for line in lines {
+        let mut open_bracket_vector = Vec::new();
+        let mut corrupted = false;
+        for c in line.chars() {
             match c {
                 '(' => open_bracket_vector.push(c),
                 '[' => open_bracket_vector.push(c),
                 '{' => open_bracket_vector.push(c),
                 '<' => open_bracket_vector.push(c),
                 _ => {
-                    if let Some(bracket) = open_bracket_vector.last() {
-                        if *bracket == return_matching_bracket(c) {
+                    if open_bracket_vector.last().unwrap() == matching_bracket.get(&c).unwrap() {
                             open_bracket_vector.pop();
-                        } else {
-                            part1_total += calculate_points_p1(c);
-                            corrupted_line[i] = true;
-                            open_bracket_vector.clear();
-                            break;
-                        }
                     } else {
-                        part1_total += calculate_points_p1(c);
-                        corrupted_line[i] = true;
-                        open_bracket_vector.clear();
+                        part1_total += part1_points.get(&c).unwrap();
+                        corrupted = true;
                         break;
                     }
                 }
             }
         }
-    }
 
-    for i in 0..lines.len() {
-        if !corrupted_line[i] {
-            for c in lines[i].chars() {
-                match c {
-                    '(' => open_bracket_vector.push(c),
-                    '[' => open_bracket_vector.push(c),
-                    '{' => open_bracket_vector.push(c),
-                    '<' => open_bracket_vector.push(c),
-                    _ => {
-                        if *open_bracket_vector.last().unwrap() == return_matching_bracket(c) {
-                            open_bracket_vector.pop();
-                        }
-                    }
-                }
-            }
-
+        if !corrupted {
             let mut part2_total: usize = 0;
-
             for _ in 0..open_bracket_vector.len() {
                 part2_total *= 5;
-                part2_total += calculate_points_p2(open_bracket_vector.pop().unwrap());
+                part2_total += part2_points.get(&open_bracket_vector.pop().unwrap()).unwrap();
             }
-
             part2_total_vec.push(part2_total);
         }
     }
-
+    
     part2_total_vec.sort();
-
-
     println!("Part 1 Score: {}", part1_total);
     println!("Part 2 Score: {}", part2_total_vec[part2_total_vec.len() / 2]);
-}
-
-fn calculate_points_p1(bracket: char) -> i32 {
-    match bracket {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        _ => panic!("Invalid bracket"),
-    }
-}
-
-fn calculate_points_p2(bracket: char) -> usize {
-    match bracket {
-        '(' => 1,
-        '[' => 2,
-        '{' => 3,
-        '<' => 4,
-        _ => panic!("Invalid bracket"),
-    }
-}
-
-fn return_matching_bracket(bracket: char) -> char {
-    match bracket {
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-        '>' => '<',
-        _ => panic!("Invalid bracket"),
-    }
 }
